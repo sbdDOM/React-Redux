500441c02deabbc5b370ba94b3d423e875398738
tsrDevTeam <tsrdevteam@gmail.com>
2023-04-12 17:21:45 +0200

    axios and redux-thunk

--- Redux/asyncActions.js
+++ Redux/asyncActions.js
@@ -1,5 +1,8 @@
 const redux = require('redux')
+const thunkMiddleware = require('redux-thunk').default
+const axios  = require('axios')
 const createStore = redux.createStore
+const applyMiddleware = redux.applyMiddleware
 
 const initialState = {
     loading: false,
@@ -53,4 +56,23 @@ const reducer = (state = initialState, action) => {
     }         
 }
 
-const store = createStore(reducer)
+const fetchUsers = () => {
+    return function(dispatch) {
+        dispatch(fetchUsersRequest())
+        axios
+            .get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                //response.data is the users 
+                const users = response.data.map((user) => user.id)
+                dispatch(fetchUsersSuccess(users))
+            }).catch(error => {
+                //error.message is the error message
+                dispatch(fetchUsersFailure(error.message))
+            })
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+
+store.subscribe(() => { console.log(store.getState())})
+store.dispatch(fetchUsers())
